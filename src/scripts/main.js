78a0d69b558804db17455d59593634a3bc0c7278
caiocroccia22 <caiocroccia22@gmail.com>
2025-03-10 20:16:46 -0300

    feat: implement counter with JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,7 @@ console.log(eventTimeStamp);
 
 // Atualizar de modo dinamico a cada 1 segundo
 const getHours = setInterval(function() {
-    
+
     // Capturar o time Stamp de Agora
     const now = new Date()
     const currentTimeStamp = now.getTime();
@@ -16,7 +16,29 @@ const getHours = setInterval(function() {
     const eventInterval = eventTimeStamp - currentTimeStamp;
 
     // Calcular o intervalo de tempo em dias
-    const daysEventInterval = eventInterval / (1000 * 60 * 60 * 24)
+    // Math.floor() para arredondar o valor para baixo
+    const daysEventInterval = Math.floor(eventInterval / (1000 * 60 * 60 * 24));
+
+    // Calcular o intervalo de tempo em horas
+    const hoursEventInterval = Math.floor((eventInterval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    
+    // Calcular o intervalo de tempo em minutos
+    const minutesEventInterval = Math.floor((eventInterval % (1000 * 60 * 60 * 24)) / (1000 * 60));
+
+    // Calcular o intervalo de tempo em segundos
+    const secondsEventInterval = Math.floor((eventInterval % (1000 * 60 * 60 * 24)) / 1000);
+    
     console.log(daysEventInterval);
+    console.log(hoursEventInterval);
+    console.log(minutesEventInterval);
+    console.log(secondsEventInterval);
+
+    document.getElementById("contador").innerHTML = daysEventInterval + ' ' + 'dias' + ' ' + hoursEventInterval + ' ' + 'horas'
+    + ' ' + minutesEventInterval + ' ' + 'minutos' +  ' ' + secondsEventInterval + ' ' + 'segundos';
+
+    if(eventInterval < 0){
+        clearInterval(eventInterval);
+        document.getElementById("contador").innerHTML = "Evento Expirado";
+    }
 
 }, 1000);
